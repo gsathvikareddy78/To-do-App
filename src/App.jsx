3e8b9874827d1a14a4b47e37dd89ef1fd5a1e3ce
sathvika <gsathvikareddy78@gmail.com>
2024-06-09 14:59:21 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./App.css";
+import InputContainer from "./components/InputContainer";
+import TodoContainer from "./components/TodoContainer";
+
+function App() {
+  const [inputVal, setInputVal] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function writeTodo(e) {
+    setInputVal(e.target.value);
+  }
+
+  function addTodo() {
+    if (inputVal != "") {
+      setTodos((prevTodos) => [...prevTodos, inputVal]);
+      setInputVal("");
+    }
+  }
+
+  console.log(todos);
+
+  function delTodo(todoIndex) {
+    setTodos((prevTodos) =>
+      prevTodos.filter((prevTodos, prevTodosIndex) => {
+        return prevTodosIndex != todoIndex;
+      })
+    );
+  }
+
+  return (
+    <main>
+      <h1>To Do List</h1>
+      <InputContainer
+        inputVal={inputVal}
+        writeTodo={writeTodo}
+        addTodo={addTodo}
+      />
+      <TodoContainer todos={todos} delTodo={delTodo} />
+    </main>
+  );
+}
+
+export default App;
